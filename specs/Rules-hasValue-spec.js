8f4e2566d55500a98e58bafc13cc4fe1c8c5dfc2
Erwan Jegouzo <erwan.jegouzo@gmail.com>
2015-04-21 15:32:13 -0400

    improving validation rules + unit tests

---
+++ specs/Rules-hasValue-spec.js
@@ -0,0 +1,54 @@
+var Formsy = require('./../src/main.js');
+
+describe('Rules: hasValue', function() {
+	var TestInput, isValid, form, input;
+
+  beforeEach(function() {
+  	isValid = jasmine.createSpy('valid');
+  	
+  	TestInput = React.createClass({
+      mixins: [Formsy.Mixin],
+      updateValue: function (event) {
+        this.setValue(event.target.value);
+      },
+      render: function () {
+        if (this.isValid()) {
+          isValid();
+        }
+        return <input value={this.getValue()} onChange={this.updateValue}/>
+      }
+    });
+
+    form = TestUtils.renderIntoDocument(
+      <Formsy.Form>
+        <TestInput name="foo" validations="hasValue"/>
+      </Formsy.Form>
+    );
+
+    input = TestUtils.findRenderedDOMComponentWithTag(form, 'INPUT');
+
+  });
+
+  afterEach(function() {
+  	TestInput = isValid = isInvalid = form = null;
+  });
+
+  it('should fail with undefined', function () {
+    expect(isValid).not.toHaveBeenCalled();
+    TestUtils.Simulate.change(input, {target: {value: undefined}});
+    expect(isValid).not.toHaveBeenCalled();
+  });
+
+  it('should fail with null', function () {
+    expect(isValid).not.toHaveBeenCalled();
+    TestUtils.Simulate.change(input, {target: {value: null}});
+    expect(isValid).not.toHaveBeenCalled();
+  });
+
+  it('should pass with a string', function () {
+    expect(isValid).not.toHaveBeenCalled();
+    TestUtils.Simulate.change(input, {target: {value: 'myValue'}});
+    expect(isValid).toHaveBeenCalled();
+  });
+
+});
