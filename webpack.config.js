6d64dd34b01d42e65a0a32a48c4b2111c141d774
christianalfoni <christianalfoni@gmail.com>
2015-04-24 08:22:09 +0200

    Cleaning up tests setup

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+var path = require('path');
+
+module.exports = {
+
+  devtool: 'inline-source-map',
+
+  entry: path.resolve(__dirname, 'build', 'test.js'),
+
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'build.js'
+  },
+
+  module: {
+    loaders: [
+      { test: /\.js$/, exclude: /node_modules/, loader: 'babel' }
+    ]
+  }
+
+};
