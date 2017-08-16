ad65d4eea8fb1132d984193646ffe35e51f78b72
Aesop Wolf <aesopwolf@gmail.com>
2017-08-16 16:39:03 -0700

    Rename webpack file and remove json loader

--- webpack.config.js
+++ webpack.config.js
@@ -15,10 +15,6 @@ module.exports = {
       test: /\.jsx?$/,
       exclude: /node_modules/,
       loader: 'babel-loader',
-    }, {
-      test: /\.json$/,
-      loader: 'json',
-    },
-    ],
+    }],
   },
 };
